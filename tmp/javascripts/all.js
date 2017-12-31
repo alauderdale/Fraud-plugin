402d97a8dbde3afc0b829e67c5838adf7b10e3d1
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2017-12-30 21:51:47 -0700

    build

--- tmp/javascripts/all.js
+++ tmp/javascripts/all.js
@@ -12157,22 +12157,22 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
 })( window );
 $(document).ready(function(){
 
-	var onText = '<p><span class="bold-font-name">You have successfully activated your Fraud Alert Cancelations!</span><br/>We will automatically cancel orders marked high risk by Shopify.</p>';
-	var offText = '<p><span class="bold-font-name">Fraud Alert Cancelations are off.</span> <br/>Click the toggle when ready to activate.</p>';
+	// var onText = '<p><span class="bold-font-name">You have successfully activated your Fraud Alert Cancelations!</span><br/>We will automatically cancel orders marked high risk by Shopify.</p>';
+	// var offText = '<p><span class="bold-font-name">Fraud Alert Cancelations are off.</span> <br/>Click the toggle when ready to activate.</p>';
 
-	$('#console-event').html(offText);
+	// $('#console-event').html(offText);
 
-    $(function() {
-    $('#activate-toggle').change(function() {
+ //    $(function() {
+ //    $('#activate-toggle').change(function() {
  
-	  if ($(this).is(':checked')) {
-	    $('#console-event').html(onText);
-	  } else {
-	    $('#console-event').html(offText);
-	  }
+	//   if ($(this).is(':checked')) {
+	//     $('#console-event').html(onText);
+	//   } else {
+	//     $('#console-event').html(offText);
+	//   }
 
-    })
-  })
+ //    })
+ //  })
 
 
 });
